bad05a3b69645be27a38ba50403790c635c668c9
Sundou94 <71121307+Sundou94@users.noreply.github.com>
2022-06-19 16:51:31 +0900

    study : theJava8 - about functional Method (2)

---
+++ src/main/java/theJava8/RamdaExpression.java
@@ -0,0 +1,17 @@
+package theJava8;
+
+import java.util.function.BinaryOperator;
+import java.util.function.IntConsumer;
+
+public class RamdaExpression {
+    /*
+    람다 표현식
+    (인자) -> 바디
+    (인자 리스트) -> {바디}
+     */
+
+    /*
+    BinaryOperator : 같은 type 의 param 2개와 같은 type 의 return 값을 가질때 사용한다
+     */
+//    BinaryOperator<Integer>
+}
